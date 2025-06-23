24a15d5c59dbd2d1ec1c12336d09489269e06c55
RynJyy <ryancanlas299@gmail.com>
2025-06-23 20:09:22 +0800

    train_model_save_history

--- image_classify.py
+++ image_classify.py
@@ -5,34 +5,44 @@ import matplotlib.pyplot as plt  # For plotting training history
 
 #class for image classification
 class ImageClassifier:
-    def __init__(self, image_size=(160, 160), batch_size=32):
+    def __init__(self, image_size=(160, 160), batch_size = 32):
         self.image_size = image_size
         self.batch_size = batch_size
         self.model = None
 
     #define method to load and validate data
     def load_data(self, train_dir, test_dir):
-        train_datagen = ImageDataGenerator(rescale=1./255)
-        test_datagen = ImageDataGenerator(rescale=1./255)
+        train_datagen = ImageDataGenerator(rescale = 1./255)
+        test_datagen = ImageDataGenerator(rescale = 1./255)
         #load train images
         self.train_generator = train_datagen.flow_from_directory(
             train_dir,
-            target_size=self.image_size,
-            batch_size=self.batch_size,
-            class_mode='binary'
+            target_size = self.image_size,
+            batch_size = self.batch_size,
+            class_mode ='binary'
         )
         #load test images
         self.test_generator = test_datagen.flow_from_directory(
             test_dir,
-            target_size=self.image_size,
-            batch_size=self.batch_size,
-            class_mode='binary'
+            target_size = self.image_size,
+            batch_size = self.batch_size,
+            class_mode = 'binary'
         )
 
         #compile the model with loss function, optimizer and metrics
         def compile_model(self):
             self.model.compile(
-            loss='binary_crossentropy',
-            optimizer='adam',
-            metrics=['accuracy']
+            loss = 'binary_crossentropy',
+            optimizer = 'adam',
+            metrics = ['accuracy']
         )
+            
+        #training model and saving the history
+        def train(self, epochs = 10, steps_per_epoch = 100, test_steps =50):
+            self.history = self.model.fit(
+                self.train_generator,
+                steps_per_epoch = steps_per_epoch,
+                epochs = epochs,
+                test_data = self.test_generator,
+                test_steps = test_steps
+            )
