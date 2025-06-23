753522b55bc0de0222a2f26a62014f2e388ddecb
RynJyy <ryancanlas299@gmail.com>
2025-06-23 20:21:24 +0800

    insert_class_check_CNN

--- cat_dog_classify.py
+++ cat_dog_classify.py
@@ -1,3 +1,22 @@
 #import necessary libraries
 from image_classify import ImageClassifier
 from tensorflow.keras import layers, models
+
+#insert ImageClassifier class
+class CatDogClassifier(ImageClassifier):
+    def build_model(self):
+        self.model = models.Sequential([
+            #check the CNN architecture
+            layers.Conv2D(32, (3, 3), activation='relu', input_shape=(*self.image_size, 3)),
+            layers.MaxPooling2D(2, 2),
+
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D(2, 2),
+
+            layers.Conv2D(128, (3, 3), activation='relu'),
+            layers.MaxPooling2D(2, 2),
+
+            layers.Flatten(),
+            layers.Dense(512, activation='relu'),
+            layers.Dense(1, activation='sigmoid')
+        ])
