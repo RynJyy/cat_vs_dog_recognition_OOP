bbbae99bdbc413246649af30175ef58678c03371
RynJyy <ryancanlas299@gmail.com>
2025-06-23 20:35:39 +0800

    fix_indentation

--- image_classify.py
+++ image_classify.py
@@ -28,23 +28,23 @@ class ImageClassifier:
             class_mode = 'binary'
         )
 
-        #compile the model with loss function, optimizer and metrics
-        def compile_model(self):
-            self.model.compile(
+    #compile the model with loss function, optimizer and metrics
+    def compile_model(self):
+        self.model.compile(
             loss = 'binary_crossentropy',
             optimizer = 'adam',
             metrics = ['accuracy']
         )
             
-        #training model and saving the history
-        def train(self, epochs = 10, steps_per_epoch = 100, test_steps =50):
-            self.history = self.model.fit(
-                self.train_generator,
-                steps_per_epoch = steps_per_epoch,
-                epochs = epochs,
-                test_data = self.test_generator,
-                test_steps = test_steps
-            )
+    #training model and saving the history
+    def train(self, epochs = 10, steps_per_epoch = 100, test_steps =50):
+        self.history = self.model.fit(
+            self.train_generator,
+            steps_per_epoch = steps_per_epoch,
+            epochs = epochs,
+            validation_data = self.test_generator,
+            validation_steps = test_steps
+        )
     #save the model
     def save_model(self, filename):
         self.model.save(filename)
