6e4a664d2ef5061343847f50c1c8a599c421d852
RynJyy <ryancanlas299@gmail.com>
2025-06-24 02:26:55 +0800

    load_model_create_GUI

--- dog_cat_classify_app.py
+++ dog_cat_classify_app.py
@@ -5,3 +5,12 @@ from tensorflow.keras.preprocessing import image
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
+
+#load model
+model = tf.keras.models.load_model('dogs_vs_cats_classifier.h5')
+
+#create GUI window
+window = tk.Tk()
+window.title("🐶 Dogs vs 🐱 Cats Classifier")
+window.geometry("600x600")
+window.resizable(False, False)
