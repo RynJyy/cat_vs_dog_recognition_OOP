c427eeafd2dd804658a01f79ababe5a751f98be0
RynJyy <ryancanlas299@gmail.com>
2025-06-23 21:02:36 +0800

    add_necessary_libraries

---
+++ dog_cat_classify_app.py
@@ -0,0 +1,7 @@
+#import necessary libraries
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
