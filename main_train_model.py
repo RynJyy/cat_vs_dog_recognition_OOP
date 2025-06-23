2810f4d9e2707f634d18feb30289dd1d52a71688
RynJyy <ryancanlas299@gmail.com>
2025-06-23 20:27:48 +0800

    main_function_loading_data

--- main_train_model.py
+++ main_train_model.py
@@ -1,3 +1,12 @@
 #import necessary class
 from cat_dog_classify import CatDogClassifier
 
+#main function to train the model
+if __name__ == "__main__":
+    #load the data
+    classifier = CatDogClassifier()
+    classifier.load_data(
+        train_dir='data/train',
+        test_dir='data/test'
+    )  
+    
